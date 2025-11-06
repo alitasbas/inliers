aff40911d29350c76c099a054a944f36135777fb
Ali Tasbas <alininkifa45@gmail.com>
2025-11-06 10:09:42 +0300

    initial commit of the project

---
+++ inlier_generation.R
@@ -0,0 +1,59 @@
+library(dplyr)
+
+set.seed(238)
+
+inlier_gen <- function(alpha, beta, n=1000, min, max) {
+  all_sample <- rnorm(n*100, alpha, beta)
+  
+  index <- ifelse(between(all_sample, alpha - beta, alpha + beta) & between(all_sample, min, max), TRUE, FALSE)
+  
+  real_sample <- sample(all_sample[index], n) # replace = FALSE or TRUE?
+  
+  return(real_sample)
+}
+
+compare_dists <- function(main_sample, inliers, ratio=0.1) {
+  n <- length(main_sample)
+  u <- as.integer(runif(n) > ratio)
+  
+  print(sum(u))
+  contaminated_sample <- u * main_sample + (1 - u) * inliers # affter this step we lose trace of inlier points
+  
+  main_dens <- density(main_sample)
+  inlier_dens <- density(inliers)
+  
+  max_y <- max(main_dens$y, inlier_dens$y)
+  
+  par(mfrow=c(1, 2))
+  
+  plot(main_dens, col = "blue", lwd = 2,
+       ylim = c(0, max_y))
+  
+  lines(inlier_dens, col = "red", lwd = 2)
+  # legend("topright", legend = c("normal", "inlier"), col = c("blue", "red"), lwd = 2)
+  
+  plot(density(c(contaminated_sample)), col="red", lwd=2)
+  lines(density(main_sample), col = "blue", lwd = 2)
+  # legend("topright", legend = c("clean", "contaminated"), col = c("blue", "red"), lwd = 2)
+}
+
+
+# Normal Sample
+
+norm_sample <- rnorm(1000, 21, 2.5)
+inliers <- inlier_gen(21, 1.25, 1000, min=20, max=22)
+
+hist(inliers)
+hist(norm_sample)
+compare_dists(norm_sample, inliers, ratio=1/10)
+
+
+# Exp Sample
+
+exp_sample <- rexp(1000, rate=1/30)
+inliers <- inlier_gen(30, 1, 1000, min=28, max=31)
+
+hist(inliers)
+hist(exp_sample)
+compare_dists(exp_sample, inliers, ratio=1/15)
+
