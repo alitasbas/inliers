e62557243ca1d584be61d809a9760c8e09f91fc6
Ali Tasbas <alininkifa45@gmail.com>
2025-11-26 23:02:09 +0300

    the function now works

--- Clustering Diffs.R
+++ Clustering Diffs.R
@@ -27,38 +27,48 @@ clusters <- function(sample) {
   colnames(diffs_table) <- c("frequency", "difference")
 
   threshold <- diffs_table[3, 2]
-  binary_diffs <- as.integer(diffs <= threshold) # 1 if the distance < threshold
+  binary_diffs <- c(as.integer(diffs <= threshold), 0, 0) # 1 if the distance < threshold
   
   # Find the number of connected data points
   clusters_df <- data.frame(lower_boundary = numeric(0),
                             upper_boundary = numeric(0),
                             cluster_size = numeric(0))
-  cluster_size <- 0
-  for (point_index in 1:length(binary_diffs)) {
+  c_size <- 0
+  next_point_index <- 0
+  for (point_index in 1:(n-1)) {
+    cat("checking index", point_index, "\n")
+    if (point_index <= next_point_index) {
+      next
+    }
     
     if (binary_diffs[point_index] == 1) {
       
-      for (next_point_index in (point_index + 1): length(binary_diffs)) {
+      for (next_point_index in (point_index + 1): (n-1)) {
+        cat("Matched. Now checking trailing indexes", next_point_index, "\n")
         
-        if (sum(binary_diffs[next_point_index:next_point_index + 2]) == 0) {
+        print(binary_diffs[next_point_index:(next_point_index + 2)])
+        print(sum(binary_diffs[next_point_index:(next_point_index + 2)]))
+        if (sum(binary_diffs[next_point_index:(next_point_index + 2)]) == 0) {
           
-          cluster_boundaries <- c(point_index, next_point_index - 1)
-          c_size <- sum(binary_diffs[point_index: next_point_index])
+          cluster_boundaries <- c(point_index, (next_point_index - 1))
+          cat("cluster_boundaries: ", cluster_boundaries, "\n")
+          c_size <- sum(binary_diffs[cluster_boundaries[1]: cluster_boundaries[2]])
           
           if (c_size < 3) {
-            cat("skipped at", point_index)
-            break
+            cat("skipped at", point_index, "\n")
+            
           }
           else {
+            print("Adding to dataframe")
             clusters_df <- add_row(clusters_df, 
-                                   lower_boundary=point_index,
-                                   upper_boundary=next_point_index - 1,
+                                   lower_boundary=cluster_boundaries[1],
+                                   upper_boundary=cluster_boundaries[2],
                                    cluster_size = c_size)
           }
-          
           break
         }
-      }
+        
+      } # end of for(next_point_index) loop 
     }
     
   }
